53d2b872c92ed9ed7fc3f49a87f8108af123f51d
jayapal <jayapal.d@gmail.com>
2020-11-30 21:30:07 +0530

    html file dev

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi welcome all!
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
