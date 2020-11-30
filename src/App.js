d28bf9abb3b791f6643a1c0ac517c7ab0301709f
jayapal <jayapal.d@gmail.com>
2020-11-30 23:50:36 +0530

    Included img font, style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
+import logo from './tarang.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to TARANG!
+        <img src={logo} alt="logo" />
+        <p className="allign">
           <br></br>
-          Click here: <a href="https://www.vodafoneidea.com/" target="_blank" >https://www.vodafoneidea.com/</a>
+          <p>Tarang 2.0 is here with an all new avatar.Here's your chance to set up and be a network ambassador.</p>
+          <p>Report and resolve network issue that you, your family or friends know about.</p>
+          <p>Let's be the change, to drive the change. Join in out commitments towards experience for all.</p>
+          <p>Click here: <a href="https://tarang.vodafoneidea.com/" target="_blank" >https://tarang.vodafoneidea.com/</a></p>
+          <p>Weekly once please raise one feedback</p>
+
+          <b>Warm Regards,</b><br></br>
+          <b>Customer service & network</b>
         </p>
 
       </header>
