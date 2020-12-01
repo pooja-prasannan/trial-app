182beaba291ea8562dcdfc529cc60394d94c40ac
jayapal <jayapal.d@gmail.com>
2020-12-01 09:53:48 +0530

    Included speel check

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
         <p className="allign">
           <br></br>
           <img src={logo} alt="logo" />
-          <p>Tarang 2.0 is here with an all new avatar.Here's your chance to set up and be a network ambassador.</p>
+          <p>Tarang 2.0 is here with an all new avatar.Here's your chance to step up and be a network ambassador.</p>
           <p>Report and resolve network issues that you, your family or friends know about.</p>
           <p>Let's be the change, to drive the change. Join us in our commitment towards creating a better network experience for all.</p>
           <p>Click here: <a href="https://tarang.vodafoneidea.com/" target="_blank" >https://tarang.vodafoneidea.com/</a></p>
           <p>Weekly once please raise one feedback..</p>
 
           <b>Warm Regards,</b><br></br>
-          <b>Customer service & network</b>
+          <b>Customer Service & Network</b>
         </p>
 
       </header>
